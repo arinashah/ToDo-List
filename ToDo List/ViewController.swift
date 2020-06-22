5f1ae72a91b29de21683714ec1e411712a1d2908
Arina Shah <Arina@Arinas-Air.attlocal.net>
2020-06-22 11:22:59 -0500

    Initial commit

---
+++ ToDo List/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDo List
+//
+//  Created by Arina Shah on 6/22/20.
+//  Copyright © 2020 Arina Shah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
